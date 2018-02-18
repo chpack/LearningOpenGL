ba3abc6ee5974b7d88ae8010a218c8f63e179f6d
yuqi chen <chenyuqide@outlook.com>
2018-02-18 11:05:13 +0800

    Add Uniform

--- include/manager.hpp
+++ include/manager.hpp
@@ -6,6 +6,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shader.hpp"
 #include "globj.hpp"
--- src/game.cpp
+++ src/game.cpp
@@ -16,7 +16,8 @@ int main()
     wmgr.addPro("data/shader/vc.glsl", "data/shader/fc.glsl");
     wmgr.addObj("data/points/configure.conf", "data/points/vertices.pnt", "data/points/indices");
 
-    while ( !glfwWindowShouldClose(wmgr.window))
+    float i = 0.0f;
+    while (!glfwWindowShouldClose(wmgr.window))
     {
         processInput(wmgr.window);
 
@@ -27,6 +28,14 @@ int main()
 
         // draw our first triangle
         // wmgr.programs[0].use();
+        glm::mat4 trans;
+        // trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+        trans = glm::rotate(trans, glm::radians(i), glm::vec3(0.0f, 0.0f, 1.0f));
+        i += 1;
+        // trans = glm::scale(trans, glm::vec3(0.5, 0.5f, 0.5));
+
+        unsigned int tfl = glGetUniformLocation(wmgr.programs[0].ID, "transform");
+        glUniformMatrix4fv(tfl, 1, GL_FALSE, glm::value_ptr(trans));
         wmgr.usePro(0);
 
         wmgr.drawObj(0);
